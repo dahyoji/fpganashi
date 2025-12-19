ecc2d878bafb8d1d45de2baab3cfb082f8c026a6
dahyoji <gkdud8429@naver.com>
2025-12-19 13:54:25 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,5 @@
 
+
 $(function () {
 
     let tl = gsap.timeline();
@@ -72,14 +73,19 @@ $(function () {
         });
 
         const MIS = new Swiper('.main_story_slide', {
-            loop: true,
+            slidesPerView: 1,
+    spaceBetween: 20,
+    nested: true,
+    touchStartPreventDefault: false,
+         loop: true,
             effect: 'fade',
             autoplay: {
                 delay: 2500,
                 disableOnInteraction: false,
             },
 
-        });
+});
+       
 
         const main_sns_slide = new Swiper('.main_sns_slide', {
             loop: true,
@@ -130,27 +136,6 @@ $(function () {
         playOnlyIfVisible: true,
     });
 
-
-    // let sw = true;
-
-    // $(function () {
-    //     $('.to_top').on('click', function () {
-    //         $('html, body').animate({ scrollTop: 0 })
-    //     });
-
-    //     $(window).on('scroll', function () {
-    //         //스크롤 된 양을 구함.
-    //         let sct = $(window).scrollTop();
-    //         console.log(sct)
-
-    //         if (sct > 400) {
-    //             $('.to_top').addClass('on')
-    //         } else {
-    //             $('.to_top').removeClass('on')
-    //         }
-    //     });
-
-    // });
 
     $(function () {
 
